3c86ea960f370b053edcf60080c0bf181ab99868
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2024-10-01 13:54:18 +0200

    s6

---
+++ personnel/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+for (int i = 1; i == 200; i++)
+{
+    Console.WriteLine("id{0}('nom{0}'),('prenom{0}')", i);
+    Console.ReadLine();
+}
+
+
+/*for (int i = 1; i == 200; i++)
+{
+    Console.WriteLine("id{0}('nom{0}'),('prenom{0}')", i);
+    Console.ReadLine();
+}*/
